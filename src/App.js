2c991c50662952175fadaf16b53cec2146e1df4e
thenq <thenq>
2022-03-20 20:10:57 +0700

    Design UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,199 @@
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='hello'>tesst</h1>
+    <div className="app-body">
+      <nav className='navbar app'>App bar</nav>
+      <nav className='navbar board'>Board bar</nav>
+      <div className='board-columns'>
+        <div className='column'>
+          <header>Brain</header>
+          <ul>
+            <li>
+              <img src="assets/thenq.jpg" />
+              Title: TheNQ
+            </li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+          </ul>
+          <footer>Add another cards</footer>
+        </div>
+        <div className='column'>
+          <header>Brain</header>
+          <ul>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+          </ul>
+          <footer>Add another cards</footer>
+        </div>
+        <div className='column'>
+          <header>Brain</header>
+          <ul>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+          </ul>
+          <footer>Add another cards</footer>
+        </div>
+        <div className='column'>
+          <header>Brain</header>
+          <ul>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+          </ul>
+          <footer>Add another cards</footer>
+        </div>
+        <div className='column'>
+          <header>Brain</header>
+          <ul>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+          </ul>
+          <footer>Add another cards</footer>
+        </div>
+        <div className='column'>
+          <header>Brain</header>
+          <ul>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+          </ul>
+          <footer>Add another cards</footer>
+        </div>
+
+        <div className='column'>
+          <header>Brain</header>
+          <ul>
+            <li>
+              <img src="https://www.facebook.com/photo/?fbid=2601127920129767&set=a.1387738991468672" />
+              Title: Thenq
+            </li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+            <li>Add work</li>
+          </ul>
+          <footer>Add another cards</footer>
+        </div>
+      </div>
     </div>
   );
 }
